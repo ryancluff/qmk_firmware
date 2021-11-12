5cad7dd952a10fbb4ae399163aa2d607f74eb04f
ryancluff <ryancluf@yahoo.com>
2021-11-11 22:20:38 -0500

    Add desktop swap on mac layer

--- keyboards/cckb/cluff81v1/keymaps/default/keymap.c
+++ keyboards/cckb/cluff81v1/keymaps/default/keymap.c
@@ -37,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,    KC_F16,
         KC_LCTL,    KC_LGUI,    KC_LALT,    KC_SPC,     KC_BSPC,    KC_DEL,     KC_RALT,    KC_RCTL,    TG(_ML)),
     [_ML] = LAYOUT(
-        KC_ESC,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,     KC_MUTE,
-        KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,    KC_F13,
-        KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_F14,
+        KC_ESC,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,     LGUI(KC_UP),
+        KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,    C(KC_RIGHT),
+        KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    C(KC_LEFT),
         MO(_TL),    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,     KC_F15,
         KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,    MOVE,
         KC_LGUI,    KC_LCTL,    KC_LALT,    KC_SPC,     KC_BSPC,    KC_DEL,     KC_RALT,    KC_RCTL,    TG(_ML)),
@@ -53,14 +53,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == 0) { /* First encoder */
-    if (clockwise) {
-      tap_code(KC_VOLU);
-    } else {
-      tap_code(KC_VOLD);
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(A(KC_TAB));
+        } else {
+            tap_code(S(A(KC_TAB)));
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
